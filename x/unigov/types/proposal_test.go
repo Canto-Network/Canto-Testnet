3551ae5f23979f363178da0387475e31f54164ba
Siddarth Vijay <vsiddarthvijay@gmail.com>
2022-06-26 20:47:29 +0530

    added tests for unigov proposals

--- x/unigov/types/proposal_test.go
+++ x/unigov/types/proposal_test.go
@@ -1,24 +1,39 @@
 package types
 
 import (
-	"fmt"
 	"github.com/cosmos/cosmos-sdk/x/gov/types"
+	"github.com/stretchr/testify/suite"
 	"reflect"
 	"testing"
 )
 
-func createFullMetadata(account string, propId uint64, values []uint64, calldatas []string, signatures []string) LendingMarketMetadata {
+type ProposalTestSuite struct {
+	suite.Suite
+}
+
+func TestProposalTestSuite(t *testing.T) {
+	suite.Run(t, new(ProposalTestSuite))
+}
+
+func (suite *ProposalTestSuite) TestKeysTypes() {
+	suite.Require().Equal("unigov", (&LendingMarketProposal{}).ProposalRoute())
+	suite.Require().Equal("Lending-Market", (&LendingMarketProposal{}).ProposalType())
+	suite.Require().Equal("unigov", (&TreasuryProposal{}).ProposalRoute())
+	suite.Require().Equal("Treasury", (&TreasuryProposal{}).ProposalType())
+}
+
+func createLendingMarketFullMetadata(account []string, propId uint64, values []uint64, calldatas []string, signatures []string) LendingMarketMetadata {
 	return LendingMarketMetadata{
-		Account:    nil,
-		PropId:     0,
-		Values:     nil,
-		Calldatas:  nil,
-		Signatures: nil,
+		Account:    account,
+		PropId:     propId,
+		Values:     values,
+		Calldatas:  calldatas,
+		Signatures: signatures,
 	}
 }
 
-func createMetadata(account string, propId uint64, values []uint64, calldatas []string, signatures []string) LendingMarketMetadata {
-	return createFullMetadata(account, propId, values, calldatas, signatures)
+func createLendingMarketMetadata(account []string, propId uint64, values []uint64, calldatas []string, signatures []string) LendingMarketMetadata {
+	return createLendingMarketFullMetadata(account, propId, values, calldatas, signatures)
 }
 
 func TestLendingMarketProposal(t *testing.T) {
@@ -37,31 +52,226 @@ func TestLendingMarketProposal(t *testing.T) {
 	}
 
 	tests := []struct {
-		name string
-		args args
-		want types.Content
+		name       string
+		args       args
+		want       types.Content
+		expectPass bool
 	}{
 		// Valid tests
 		{
-			name: "Test",
+			name: "Valid Test",
 			args: struct {
 				title       string
 				description string
 				m           *LendingMarketMetadata
 			}{
 				title:       "Test",
-				description: "Test",
+				description: "Valid",
 				m:           &validMetaData,
 			},
-			want: nil,
+			want:       nil,
+			expectPass: false,
 		},
+		// Invalid tests
+		//{
+		//	name: "Invalid Test",
+		//	args: struct {
+		//		title       string
+		//		description string
+		//		m           *LendingMarketMetadata
+		//	}{
+		//		title:       "Test",
+		//		description: "Invalid",
+		//		m:           createLendingMarketMetadata([]string{}, 0, []uint64{}, []string{}, []string{}),
+		//	},
+		//	want: nil,
+		//	expectPass: true,
+		//},
 	}
 
 	for _, tt := range tests {
-		fmt.Println(NewLendingMarketProposal(tt.args.title, tt.args.description, tt.args.m))
 		t.Run(tt.name, func(t *testing.T) {
-			if got := NewLendingMarketProposal(tt.args.title, tt.args.description, tt.args.m); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewLendingMarketProposal() = %v, want %v", got, tt.want)
+			tx := NewLendingMarketProposal(tt.args.title, tt.args.description, tt.args.m)
+			e := tx.ValidateBasic()
+
+			if tt.expectPass {
+				if e != nil {
+					t.Errorf("Error: %v", e.Error())
+				}
+			} else {
+				if e == nil {
+					t.Errorf("Error: %v", e.Error())
+				}
+			}
+
+			if !reflect.DeepEqual(tx, tt.want) {
+				t.Errorf("NewLendingMarketProposal() = %v, want %v", tx, tt.want)
+			}
+		})
+	}
+}
+
+func createTreasuryFullMetadata(propId uint64, recipient string, amount uint64, denom string) TreasuryProposalMetadata {
+	return TreasuryProposalMetadata{
+		PropID:    propId,
+		Recipient: recipient,
+		Amount:    amount,
+		Denom:     denom,
+	}
+}
+
+func createTreasuryMetadata(propId uint64, recipient string, amount uint64, denom string) TreasuryProposalMetadata {
+	return createTreasuryMetadata(propId, recipient, amount, denom)
+}
+
+func TestNewTreasuryProposal(t *testing.T) {
+	validMetaData := TreasuryProposalMetadata{
+		PropID:    0,
+		Recipient: "",
+		Amount:    0,
+		Denom:     "",
+	}
+
+	type args struct {
+		title       string
+		description string
+		tm          *TreasuryProposalMetadata
+	}
+
+	tests := []struct {
+		name       string
+		args       args
+		want       types.Content
+		expectPass bool
+	}{
+		// Valid tests
+		{
+			name: "Valid Test",
+			args: struct {
+				title       string
+				description string
+				tm          *TreasuryProposalMetadata
+			}{
+				title:       "Test",
+				description: "Valid",
+				tm:          &validMetaData,
+			},
+			want:       nil,
+			expectPass: false,
+		},
+		// Invalid tests
+		//{
+		//	name: "Invalid Test",
+		//	args: struct {
+		//		title       string
+		//		description string
+		//		tm           *TreasuryProposalMetaData
+		//	}{
+		//		title:       "Test",
+		//		description: "Invalid",
+		//		tm:           createLendingMarketMetadata([]string{}, 0, []uint64{}, []string{}, []string{}),
+		//	},
+		//	want: nil,
+		//	expectPass: true,
+		//},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx := NewTreasuryProposal(tt.args.title, tt.args.description, tt.args.tm)
+			e := tx.ValidateBasic()
+
+			if tt.expectPass {
+				if e != nil {
+					t.Errorf("Error: %v", e.Error())
+				}
+			} else {
+				if e == nil {
+					t.Errorf("Error: %v", e.Error())
+				}
+			}
+
+			if !reflect.DeepEqual(tx, tt.want) {
+				t.Errorf("NewTreasuryProposal() = %v, want %v", tx, tt.want)
+			}
+		})
+	}
+}
+
+func TestTreasuryProposal_FromTreasuryToLendingMarket(t *testing.T) {
+	validMetaData := TreasuryProposalMetadata{
+		PropID:    0,
+		Recipient: "",
+		Amount:    0,
+		Denom:     "",
+	}
+
+	type args struct {
+		title       string
+		description string
+		m           *TreasuryProposalMetadata
+	}
+
+	tests := []struct {
+		name       string
+		args       args
+		want       *LendingMarketProposal
+		expectPass bool
+	}{
+		// Valid tests
+		{
+			name: "Valid Test",
+			args: struct {
+				title       string
+				description string
+				m           *TreasuryProposalMetadata
+			}{
+				title:       "Test",
+				description: "Valid",
+				m:           &validMetaData,
+			},
+			want:       nil,
+			expectPass: false,
+		},
+		// Invalid tests
+		//{
+		//	name: "Invalid Test",
+		//	args: struct {
+		//		title       string
+		//		description string
+		//		m           *LendingMarketMetadata
+		//	}{
+		//		title:       "Test",
+		//		description: "Invalid",
+		//		m:           createLendingMarketMetadata([]string{}, 0, []uint64{}, []string{}, []string{}),
+		//	},
+		//	want: nil,
+		//	expectPass: true,
+		//},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tp := &TreasuryProposal{
+				Title:       tt.args.title,
+				Description: tt.args.description,
+				Metadata:    tt.args.m,
+			}
+			tx := tp.FromTreasuryToLendingMarket()
+			e := tx.ValidateBasic()
+
+			if tt.expectPass {
+				if e != nil {
+					t.Errorf("Error: %v", e.Error())
+				}
+			} else {
+				if e == nil {
+					t.Errorf("Error: %v", e.Error())
+				}
+			}
+
+			if !reflect.DeepEqual(tx, tt.want) {
+				t.Errorf("FromTreasuryToLendingMarket() = %v, want %v", tx, tt.want)
 			}
 		})
 	}
